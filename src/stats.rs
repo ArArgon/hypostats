f8ce4e119170deb1fb449cf22954ce21171a8086
ArArgon <liaozping@gmail.com>
2025-04-07 21:52:07 -0400

    feat: hook relation information getter

--- src/stats.rs
+++ src/stats.rs
@@ -1,1 +1,12 @@
-pub mod pg_class;
+use pgrx::pg_sys::Oid;
+use pgrx::PostgresType;
+use serde::{Deserialize, Serialize};
+
+#[derive(PostgresType, Serialize, Deserialize, Debug)]
+pub(crate) struct PostgresClassStat {
+    pub relname: String,
+    pub relnamespace: Oid,
+    pub reltuples: f32,
+    pub relpages: i32,
+    pub relallvisible: i32,
+}
